6cf01df53e1be8b7db0e1a0cfc3ffa073d389663
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-18 22:07:02 +0530

    testing with mocha

---
+++ Section-2/inbox/test/inbox.test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());  //communication-layer
+ 
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+
+// describe('Car Test', () => {
+//     it('can park', () => {
+//         const car = new Car();
+//         assert.equal(car.park(), 'stopped');
+//     });
+// });
+describe('Web3', function() {
+
+  it("should load 'ganache' provider", function() {
+    assert.ok(ganache);
+  });
+
+  it("should load constructor", function() {
+    assert.ok(Web3);
+  });
+
+  it('should create an instance', function() {
+    console.log(web3);
+    assert.ok(web3);
+  });
+});
