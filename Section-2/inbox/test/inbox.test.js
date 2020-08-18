b1dcbfa7d1406c88456a8fd4cec5266cb88a9855
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-18 23:44:22 +0530

    asserting deployment

--- Section-2/inbox/test/inbox.test.js
+++ Section-2/inbox/test/inbox.test.js
@@ -1,20 +1,28 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+// const provider = ganache.provider();
+// const web3 = new Web3(provider);
 const web3 = new Web3(ganache.provider());  //communication-layer
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     //getting list of all accounts
     accounts = await web3.eth.getAccounts();
     //using one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))  //Contract() is an instance
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })  //arguments value goes to the constructor in inbox.sol
+    .send({ from: accounts[0], gas: '1000000' })
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        assert.ok(inbox.options.address);  //ok() checks whether the passed value exists or not
     });
+    // inbox.setProvider(provider);
 });
 
 // class Car {
