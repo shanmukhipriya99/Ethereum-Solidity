0e4ab73926dc9b43c1317e91935d5e9b5bb3bd2d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-18 23:13:16 +0530

    getting list of accounts

--- Section-2/inbox/test/inbox.test.js
+++ Section-2/inbox/test/inbox.test.js
@@ -3,23 +3,40 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());  //communication-layer
  
-class Car {
-    park() {
-        return 'stopped';
-    }
-    drive() {
-        return 'vroom';
-    }
-}
-let car;  //initialized here to make it global
 beforeEach(() => {
-    car = new Car();
-});
-describe('Car Test', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
+    //getting list of all accounts
+    web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+        console.log(fetchedAccounts);
     });
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
+    //using one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        
     });
 });
+
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+// let car;  //initialized here to make it global
+// beforeEach(() => {
+//     car = new Car();
+// });
+// describe('Car Test', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     });
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
+
+
