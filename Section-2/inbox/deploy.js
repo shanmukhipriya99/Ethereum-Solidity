3115e410b7b80e924450a4ec812eeb5dbde8440f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-19 12:05:06 +0530

    deploy.js

---
+++ Section-2/inbox/deploy.js
@@ -0,0 +1,19 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'test boost nuclear avoid episode inject gaze joke infant poet raven inspire',
+    'https://rinkeby.infura.io/v3/0c2d2d6254ce4b34a75eb8b6ec0ac343'
+); 
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from: ', accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+    console.log('Contract deployed to: ', result.options.address);
+};
+deploy();
