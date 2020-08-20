b71f06eaf061adb15bf60eb645188725f0cfe50f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-21 00:12:21 +0530

    test for entering the lottery

--- Section-3/lottery/test/lottery.test.js
+++ Section-3/lottery/test/lottery.test.js
@@ -10,14 +10,23 @@ let accounts;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
-  lottery = await new web3.eth
-    .Contract(JSON.parse(interface))
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe('Lottery Contract', () => {
-    it('deploys a contract', () => {
-        assert.ok(lottery.options.address);
-    });
+describe("Lottery Contract", () => {
+  it("deploys a contract", () => {
+    assert.ok(lottery.options.address);
+  });
+  it("allows one account to enter", async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[0], value: web3.utils.toWei("0.02", "ether") });
+      const players = await lottery.methods.getPlayers().call({  //calling or reaching a contract is asynchronous
+          from: accounts[0]
+      });
+      assert.equal(accounts[0], players[0]);
+      assert.equal(1, players.length);
+  });
 });
