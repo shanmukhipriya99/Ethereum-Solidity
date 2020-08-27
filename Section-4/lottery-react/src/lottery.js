96a8cf8cf2afaa763914e4fb660d0d8bfc67fca5
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-27 13:34:41 +0530

    created lottery.js

---
+++ Section-4/lottery-react/src/lottery.js
@@ -0,0 +1,61 @@
+import web3 from "./web3";
+
+const address = "0x22cbF6ca541f3aBEE0270b80d29cb850A2672d8D";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+// const contract = new web3.eth.Contract(abi, address);
+// console.log(contract);
+
+export default new web3.eth.Contract(abi, address);
