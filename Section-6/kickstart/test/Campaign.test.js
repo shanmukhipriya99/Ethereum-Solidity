ce05249047b4a2ac8d6781941ad7e595308bea68
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-10 22:49:26 +0530

    manager making a request

--- Section-6/kickstart/test/Campaign.test.js
+++ Section-6/kickstart/test/Campaign.test.js
@@ -40,5 +40,36 @@ describe('Campaigns', () => {
         const manager = await campaign.methods.manager().call();
         assert.equal(accounts[0], manager);
     });
+
+    it('allows people to contribute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+        const isContributer = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributer);
+    });    
+
+    it('requires a min contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '5',
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    it('allows a manager to make a payment request', async () => {
+        await campaign.methods.createRequest('buy battery', '100', accounts[1])
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+        const request = await campaign.methods.requests(0).call();
+        assert.equal('buy battery', request.description);
+    });
 });
 
