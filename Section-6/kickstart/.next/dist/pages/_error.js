879d20a07b54d443eaf9ff6b65d3dbd5a41b259d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-14 22:39:29 +0530

    .next folder

---
+++ Section-6/kickstart/.next/dist/pages/_error.js
@@ -0,0 +1,4 @@
+'use strict';
+
+module.exports = require('next\\dist\\lib\\error.js');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlc1xcbmV4dFxcZGlzdFxccGFnZXNcXF9lcnJvci5qcyJdLCJuYW1lcyI6WyJtb2R1bGUiLCJleHBvcnRzIiwicmVxdWlyZSJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoiX2Vycm9yLmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkM6L1VzZXJzL1NoYW5tdWtoaSBQcml5YS9Tb2xpZGl0eS9FdGhlcmV1bSZTb2xpZGl0eS9TZWN0aW9uLTYva2lja3N0YXJ0In0=
---
+++ Section-6/kickstart/.next/dist/pages/newcampaign.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'C:\\Users\\Shanmukhi Priya\\Solidity\\Ethereum&Solidity\\Section-6\\kickstart\\pages\\newcampaign.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 4
+        }
+    }, 'This is the new campaign page!!!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxuZXdjYW1wYWlnbi5qcyJdLCJuYW1lcyI6WyJSZWFjdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTyxBQUVQOzs7Ozs7OztrQkFBZSxZQUFNLEFBQ2pCOzJCQUFPLGNBQUE7O3NCQUFBO3dCQUFBO0FBQUE7QUFBQSxLQUFBLEVBQVAsQUFBTyxBQUNWO0FBRkQiLCJmaWxlIjoibmV3Y2FtcGFpZ24uanM/ZW50cnkiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvU2hhbm11a2hpIFByaXlhL1NvbGlkaXR5L0V0aGVyZXVtJlNvbGlkaXR5L1NlY3Rpb24tNi9raWNrc3RhcnQifQ==
