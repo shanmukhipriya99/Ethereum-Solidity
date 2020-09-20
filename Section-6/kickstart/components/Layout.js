486c504d20cf3e3994bdcbe516e84d197480fae6
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-20 22:11:18 +0530

    Used next for Head

--- Section-6/kickstart/components/Layout.js
+++ Section-6/kickstart/components/Layout.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import Header from './Header';
-import { Container } from 'semantic-ui-react';
+import React from "react";
+import Header from "./Header";
+import { Container } from "semantic-ui-react";
+import Head from "next/head";
 
-export default props => {
-    return (
-        <Container>
-            <Header />
-            {props.children}
-        </Container>
-    );
+export default (props) => {
+  return (
+    <Container>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+        />
+      </Head>
+      <Header />
+      {props.children}
+    </Container>
+  );
 };
--- Section-6/kickstart/pages/index.js
+++ Section-6/kickstart/pages/index.js
@@ -24,10 +24,6 @@ class CampaignIndex extends Component {
     return (
       <Layout>
         <div>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
-          />
           <h3>Open Campaigns</h3>
           <Button floated="right" content="Create Campaign" icon="add circle" primary />
           {this.renderCampaigns()}
