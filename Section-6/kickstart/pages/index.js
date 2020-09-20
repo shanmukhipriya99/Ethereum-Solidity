a8c651e81f3b18350511d1ff1a7d422df4092dcf
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-20 17:40:05 +0530

    1st deployed campaign instance

---
+++ Section-6/kickstart/pages/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import factory from '../ethereum/factory';
+
+class CampaignIndex extends Component {
+    async componentDidMount() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        console.log(campaigns);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Campaigns Index!</h1>
+                {/* <p>{campaigns}</p> */}
+            </div>
+        );
+    }
+}
+
+export default CampaignIndex;
