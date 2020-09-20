b618b07ef0383acbd355742267aa3bc357fca874
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-20 17:40:49 +0530

    Sorted window not found error

--- Section-6/kickstart/ethereum/web3.js
+++ Section-6/kickstart/ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // in the browser and metamask is running
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // in the server OR user is not running metamask
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/0c2d2d6254ce4b34a75eb8b6ec0ac343'
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
