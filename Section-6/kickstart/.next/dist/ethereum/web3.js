e7d3c7136fab43dafed7d6ae9b644dc348501c01
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-20 17:41:33 +0530

    console.log the 1st deployed contract

---
+++ Section-6/kickstart/.next/dist/ethereum/web3.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _web = require('web3');
+
+var _web2 = _interopRequireDefault(_web);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var web3 = void 0;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // in the browser and metamask is running
+    web3 = new _web2.default(window.web3.currentProvider);
+} else {
+    // in the server OR user is not running metamask
+    var provider = new _web2.default.providers.HttpProvider('https://rinkeby.infura.io/v3/0c2d2d6254ce4b34a75eb8b6ec0ac343');
+    web3 = new _web2.default(provider);
+}
+
+exports.default = web3;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImV0aGVyZXVtXFx3ZWIzLmpzIl0sIm5hbWVzIjpbIldlYjMiLCJ3ZWIzIiwid2luZG93IiwiY3VycmVudFByb3ZpZGVyIiwicHJvdmlkZXIiLCJwcm92aWRlcnMiLCJIdHRwUHJvdmlkZXIiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU8sQUFBUDs7Ozs7O0FBRUEsSUFBSSxZQUFKOztBQUVBLElBQUksT0FBTyxBQUFQLFdBQWtCLEFBQWxCLGVBQWlDLE9BQU8sT0FBTyxBQUFkLFNBQXVCLEFBQTVELGFBQXlFLEFBQ3JFO0FBQ0E7V0FBTyxBQUFJLEFBQUosa0JBQVMsT0FBTyxBQUFQLEtBQVksQUFBckIsQUFBUCxBQUNIO0FBSEQsT0FHTyxBQUNIO0FBQ0E7UUFBTSxXQUFXLElBQUksY0FBSyxBQUFMLFVBQWUsQUFBbkIsYUFDYixBQURhLEFBQWpCLEFBR0E7V0FBTyxBQUFJLEFBQUosa0JBQVMsQUFBVCxBQUFQLEFBQ0g7QUFFRDs7a0JBQWUsQUFBZiIsImZpbGUiOiJ3ZWIzLmpzIiwic291cmNlUm9vdCI6IkM6L1VzZXJzL1NoYW5tdWtoaSBQcml5YS9Tb2xpZGl0eS9FdGhlcmV1bSZTb2xpZGl0eS9TZWN0aW9uLTYva2lja3N0YXJ0In0=
