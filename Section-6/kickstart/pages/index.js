e54772cfba47f5c4a0fcf49b6d0d53c19ebcfdd2
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-20 18:12:36 +0530

    rendering card.group

--- Section-6/kickstart/pages/index.js
+++ Section-6/kickstart/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
+import { Card } from 'semantic-ui-react';
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
@@ -7,11 +8,22 @@ class CampaignIndex extends Component {
         return { campaigns };
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        });
+        return <Card.Group items = {items} />;
+    }
+
     render() {
         return (
             <div>
                 <h1>Campaigns Index!</h1>
-                <p>{this.props.campaigns[0]}</p>
+                {this.renderCampaigns()}
             </div>
         );
     }
