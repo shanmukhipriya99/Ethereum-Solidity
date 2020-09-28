e5fd2b79cd5bf8d7267e248708dc8adea97f435e
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-28 14:51:24 +0530

    summary object

--- Section-6/kickstart/pages/campaigns/show.js
+++ Section-6/kickstart/pages/campaigns/show.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
+import Campaign from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
     static async getInitialProps(props) {
-        console.log(props.query.address);
-        return {};
+        const campaign = Campaign(props.query.address);
+        const summary = await campaign.methods.getSummary().call();
+        // console.log(summary);
+        return { 
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+         };
     }
 
   render() {
