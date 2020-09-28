ac1770fd14301f4cceeff0ae6c29ab9e0471448c
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-28 12:15:46 +0530

    header navigation

--- Section-6/kickstart/components/Header.js
+++ Section-6/kickstart/components/Header.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import React from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "../routes";
 
 export default () => {
-    return (
-        <Menu style={{ marginTop: '10px'}}>
-            <Menu.Item>
-                CrowdCoin
-            </Menu.Item>
-            <Menu.Menu position='right'>
-                <Menu.Item>
-                    Campaigns
-                </Menu.Item>
-                <Menu.Item>
-                    +
-                </Menu.Item>
-            </Menu.Menu>
-        </Menu>
-    );
+  return (
+    <Menu style={{ marginTop: "10px" }}>
+      <Link route="/">
+        <a className="item">CrowdCoin</a>
+      </Link>
+      <Menu.Menu position="right">
+      <Link route="/">
+        <a className="item">Campiagns</a>
+      </Link>
+      <Link route="/campaigns/new">
+        <a className="item">+</a>
+      </Link>
+      </Menu.Menu>
+    </Menu>
+  );
 };
